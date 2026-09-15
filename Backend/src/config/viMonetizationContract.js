598e6d1354cba7f607fb51f1cb16c7fd23ad677e
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:12:17 -0700

    Define traveler-safe Vi monetization contract

---
+++ Backend/src/config/viMonetizationContract.js
@@ -0,0 +1,9 @@
+export const VI_MONETIZATION_CONTRACT = Object.freeze({
+  attributionPath: ['source', 'landing_page', 'vi_intent', 'search', 'provider', 'booking_click', 'conversion', 'revenue'],
+  providerMetadata: ['provider', 'vertical', 'destination', 'price', 'subid_or_campaign_when_supported'],
+  rankTravelerValueFirst: true,
+  monetizeSuitableOptions: true,
+  neverAlterFactualGuidanceForCommission: true,
+});
+
+export default VI_MONETIZATION_CONTRACT;
