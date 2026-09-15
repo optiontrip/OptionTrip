e381962555322c6c372b1c5d2c316fa43d122bcf
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:09:03 -0700

    Define Vi live-trip priorities

---
+++ Backend/src/config/viLiveTripPriorities.js
@@ -0,0 +1,12 @@
+export const VI_LIVE_TRIP_PRIORITIES = Object.freeze([
+  'emergency_and_safety',
+  'disruption_and_replanning',
+  'time_critical_transport',
+  'airport_station_and_baggage',
+  'weather_and_local_conditions',
+  'nearby_and_open_now',
+  'itinerary_optimization',
+  'commercial_opportunities',
+]);
+
+export default VI_LIVE_TRIP_PRIORITIES;
