ed5e1929293568c172c5f100066a2c3a1c1a3505
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:07:55 -0700

    Define Vi traveler-value ranking factors

---
+++ Backend/src/config/viTravelerValueRanking.js
@@ -0,0 +1,17 @@
+export const VI_TRAVELER_VALUE_FACTORS = Object.freeze([
+  'intent_fit',
+  'total_cost',
+  'travel_time',
+  'stops',
+  'baggage_and_fare_restrictions',
+  'cancellation_and_refundability',
+  'location',
+  'transfer_burden',
+  'accessibility',
+  'entry_and_safety_constraints',
+  'convenience',
+]);
+
+export const VI_COMMERCIAL_RANKING_RULE = 'commission_may_break_ties_among_suitable_options_but_must_not_override_material_traveler_value';
+
+export default VI_TRAVELER_VALUE_FACTORS;
