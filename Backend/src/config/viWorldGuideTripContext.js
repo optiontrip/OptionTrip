2abbde25beed2582bca8775c7de5d46ed4a0e661
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:17:44 -0700

    Define Vi trip context priority

---
+++ Backend/src/config/viWorldGuideTripContext.js
@@ -0,0 +1,11 @@
+export const VI_WORLD_GUIDE_TRIP_CONTEXT_PRIORITY = Object.freeze([
+  'active_trip_object',
+  'current_conversation',
+  'traveler_preferences',
+  'past_trip_history',
+  'live_location_when_permission_available',
+  'live_weather_and_status_when_available',
+  'provider_capabilities',
+]);
+
+export default VI_WORLD_GUIDE_TRIP_CONTEXT_PRIORITY;
