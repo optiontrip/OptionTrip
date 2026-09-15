eea030889e23b4fb5a39a9087efa25a2f10471ae
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:08:55 -0700

    Add Vi commercial integrity contract

---
+++ Backend/src/config/viCommercialIntegrity.js
@@ -0,0 +1,9 @@
+export const VI_COMMERCIAL_INTEGRITY = Object.freeze({
+  affiliateCatalogIsNotLiveInventory: true,
+  readinessCandidateIsNotAvailability: true,
+  credentialsMustRemainOutsideRepository: true,
+  attributionRequiredForCommercialPaths: true,
+  travelerValueMustNotBeOverriddenByCommission: true,
+});
+
+export default VI_COMMERCIAL_INTEGRITY;
