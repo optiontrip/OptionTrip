089fab6e06144d225439dad0e590a783cdbf1246
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:18:49 -0700

    Define Vi traveler experience principles

---
+++ Backend/src/config/viWorldGuideExperience.js
@@ -0,0 +1,12 @@
+export const VI_WORLD_GUIDE_EXPERIENCE = Object.freeze({
+  oneConversationAcrossTrip: true,
+  destinationNotRequiredToStart: true,
+  askOnlyBlockingQuestions: true,
+  doNotRepeatKnownQuestions: true,
+  proactiveButNotSpammy: true,
+  showRelevantServicesNotEveryService: true,
+  explainTradeoffs: true,
+  keepLiveFactsTraceableToProviders: true,
+});
+
+export default VI_WORLD_GUIDE_EXPERIENCE;
