51101da88bbe6b049d9ff11d733c03c585dda925
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:07:05 -0700

    Define Vi trip continuity fields

---
+++ Backend/src/config/viTripContinuity.js
@@ -0,0 +1,13 @@
+export const VI_TRIP_CONTINUITY = Object.freeze({
+  tripObject: [
+    'origin', 'destination', 'dates', 'guests', 'trip_type', 'budget',
+    'selectedFlight', 'selectedHotel', 'selectedCar', 'notes', 'travel_status'
+  ],
+  futureServiceSelections: [
+    'rail', 'bus', 'transfer', 'activity', 'tour', 'guide', 'esim', 'insurance'
+  ],
+  memorySurfaces: ['my_trips', 'travel_map', 'trip_story'],
+  privacyRequired: true,
+});
+
+export default VI_TRIP_CONTINUITY;
