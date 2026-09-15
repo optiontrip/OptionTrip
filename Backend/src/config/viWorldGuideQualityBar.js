c7e7a01f51f9789780235d0525fa18f4eba32916
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:14:40 -0700

    Define Vi world-guide quality bar

---
+++ Backend/src/config/viWorldGuideQualityBar.js
@@ -0,0 +1,13 @@
+export const VI_WORLD_GUIDE_QUALITY_BAR = Object.freeze({
+  usefulWithoutDestination: true,
+  wholeTripAware: true,
+  realDataForLiveClaims: true,
+  remembersKnownContext: true,
+  liveTripCapable: true,
+  postTripContinuity: true,
+  privacyAware: true,
+  monetizationWithoutTrustTradeoff: true,
+  automationFirst: true,
+});
+
+export default VI_WORLD_GUIDE_QUALITY_BAR;
