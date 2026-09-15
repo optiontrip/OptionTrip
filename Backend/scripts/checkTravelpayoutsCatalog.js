cdc3938eb8b08dd45b468adf5ed73300d9e4a6ab
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:50:11 -0700

    Add consolidated Travelpayouts catalog checks

---
+++ Backend/scripts/checkTravelpayoutsCatalog.js
@@ -0,0 +1,7 @@
+import './checkTravelProviderCatalog.js';
+import './checkTravelpayoutsAliases.js';
+import './checkTravelpayoutsProgramGroups.js';
+import './checkTravelpayoutsProgramPolicy.js';
+import './checkTravelpayoutsRolloutSummary.js';
+
+console.log('All Travelpayouts catalog checks passed.');
