ee237e9e5078f17245f7c4c9cbfc4732fb4012dc
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:15:42 -0700

    Add provider expansion priority tiers

---
+++ Backend/src/config/viWorldGuideProviderExpansion.js
@@ -0,0 +1,8 @@
+export const VI_PROVIDER_EXPANSION_TIERS = Object.freeze({
+  tier1_ready_targets: ['omio', 'airalo', 'tiqets', 'wegotrip', 'viator', 'gettransfer'],
+  tier2_commerce: ['rental_cars', 'insurance', 'package_tours', 'cruises'],
+  tier3_trip_support: ['ferries', 'lounges', 'parking', 'luggage_services', 'events'],
+  tier4_specialized: ['rv_camping', 'ski', 'bikes_micromobility'],
+});
+
+export default VI_PROVIDER_EXPANSION_TIERS;
