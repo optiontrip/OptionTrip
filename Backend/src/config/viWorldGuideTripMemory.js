5d9f525e94bcce99e39cf485b2ed5f73a3dce4c8
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:18:23 -0700

    Define privacy-aware Vi trip memory signals

---
+++ Backend/src/config/viWorldGuideTripMemory.js
@@ -0,0 +1,13 @@
+export const VI_WORLD_GUIDE_TRIP_MEMORY_SIGNALS = Object.freeze([
+  'destinations_visited',
+  'routes_taken',
+  'travel_styles_enjoyed',
+  'budget_preferences',
+  'stay_preferences',
+  'transport_preferences',
+  'activities_enjoyed',
+  'dietary_and_accessibility_preferences',
+  'traveler_feedback',
+]);
+
+export default VI_WORLD_GUIDE_TRIP_MEMORY_SIGNALS;
