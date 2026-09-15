ef61ddff015310daec7cc8a06fd5168733477e4b
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:14:59 -0700

    Define Vi travel telemetry events

---
+++ Backend/src/config/viWorldGuideTelemetry.js
@@ -0,0 +1,14 @@
+export const VI_WORLD_GUIDE_TELEMETRY_EVENTS = Object.freeze([
+  'vi_discovery_started',
+  'vi_destination_candidate_selected',
+  'vi_live_search_started',
+  'vi_provider_result_shown',
+  'vi_provider_result_clicked',
+  'vi_service_attached_to_trip',
+  'vi_live_trip_assist',
+  'vi_trip_completed',
+  'vi_travel_map_updated',
+  'vi_return_trip_started',
+]);
+
+export default VI_WORLD_GUIDE_TELEMETRY_EVENTS;
