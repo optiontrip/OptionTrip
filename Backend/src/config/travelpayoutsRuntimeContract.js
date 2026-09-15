39bae4d35d1faa92cc5d49073438de99c59ca72b
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:58:11 -0700

    Add Travelpayouts runtime contract

---
+++ Backend/src/config/travelpayoutsRuntimeContract.js
@@ -0,0 +1,7 @@
+export const TRAVELPAYOUTS_RUNTIME_CONTRACT = Object.freeze({
+  catalogAvailabilityIsLiveInventory: false,
+  requireConfiguredProviderForLiveExposure: true,
+  allowGuessedAffiliateUrls: false,
+  allowSecretsInRepository: false,
+  persistSelectedServicesToTripObject: true,
+});
