59f485bccbd9e6e2fe09733636a15285c6211867
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:48:38 -0700

    Group Travelpayouts programs by traveler need

---
+++ Backend/src/config/travelpayoutsProgramGroups.js
@@ -0,0 +1,10 @@
+export const TRAVELPAYOUTS_PROGRAM_GROUPS = Object.freeze({
+  transport: ['travelpayouts', 'tripcom', 'twelvego', 'kiwi'],
+  stays: ['travelpayouts', 'tripcom', 'supertravel'],
+  groundMobility: ['twelvego', 'qeeq', 'economybookings', 'bikesbooking', 'gettransfer', 'kiwitaxi', 'klook', 'welcomepickups', 'getrentacar', 'autoeurope', 'intuitravel'],
+  experiences: ['tiqets', 'klook', 'gocity', 'wegotrip', 'kkday', 'youtravel', 'viator'],
+  connectivity: ['airalo', 'drimsim', 'saily'],
+  protection: ['insubuy', 'ekta'],
+  disruptionHelp: ['airhelp', 'compensair'],
+  tripUtilities: ['radicalstorage'],
+});
