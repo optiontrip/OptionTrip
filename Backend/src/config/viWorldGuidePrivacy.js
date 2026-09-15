5f4d95d406c90b33eaf9edf0a09ec355d6cbf4ef
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:15:09 -0700

    Add Vi travel privacy principles

---
+++ Backend/src/config/viWorldGuidePrivacy.js
@@ -0,0 +1,9 @@
+export const VI_WORLD_GUIDE_PRIVACY = Object.freeze({
+  tripMemoryUserControlled: true,
+  travelMapUserControlled: true,
+  publicSharingOptIn: true,
+  sensitiveContextNotRequiredForCommercialRanking: true,
+  deleteAndAccountControlsMustBeRespected: true,
+});
+
+export default VI_WORLD_GUIDE_PRIVACY;
