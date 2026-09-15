da3cbf9be35c9b1d596b9abcbe76b7e5aab89c36
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:14:07 -0700

    Add Vi trip lifecycle states

---
+++ Backend/src/config/viWorldGuideTripStates.js
@@ -0,0 +1,12 @@
+export const VI_WORLD_GUIDE_TRIP_STATES = Object.freeze({
+  idea: 'discover',
+  draft: 'plan',
+  comparing: 'compare',
+  booking: 'book',
+  upcoming: 'prepare',
+  active: 'travel',
+  completed: 'remember',
+  historical: 'return',
+});
+
+export default VI_WORLD_GUIDE_TRIP_STATES;
