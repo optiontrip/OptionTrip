cb4d2ff69e6ae4374e8dd3e64be8e8e5e5d3cdaf
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:08:07 -0700

    Define OptionTrip whole-trip service universe

---
+++ Backend/src/config/viServiceUniverse.js
@@ -0,0 +1,11 @@
+export const VI_SERVICE_UNIVERSE = Object.freeze({
+  transport: ['flights', 'rail', 'bus', 'rental_car', 'transfer', 'taxi', 'ferry', 'bike', 'micromobility'],
+  stay: ['hotel', 'hostel', 'apartment', 'alternative_accommodation', 'camping', 'rv'],
+  experience: ['tour', 'activity', 'attraction', 'local_guide', 'event', 'ski'],
+  trip_support: ['esim', 'insurance', 'visa_entry', 'weather', 'safety', 'baggage', 'lounge', 'parking', 'luggage_storage', 'luggage_delivery', 'compensation'],
+  food: ['restaurant_discovery', 'restaurant_reservation'],
+  package: ['package_tour', 'cruise'],
+  memory: ['my_trips', 'travel_map', 'trip_story', 'wishlist'],
+});
+
+export default VI_SERVICE_UNIVERSE;
