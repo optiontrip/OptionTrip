d163327f7ac6c9209abfc5b5da5d1f004de3f22d
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:57:19 -0700

    Add Travelpayouts catalog entry point

---
+++ Backend/src/config/travelpayoutsCatalog.js
@@ -0,0 +1,5 @@
+export * from './travelpayoutsRollout.js';
+export { TRAVELPAYOUTS_CATALOG_VERSION } from './travelpayoutsCatalogVersion.js';
+export { getTravelpayoutsCatalogIntegrity } from './travelpayoutsCatalogIntegrity.js';
+export { getTravelpayoutsProgramExposure } from './travelpayoutsProgramPolicy.js';
+export { normalizeTravelpayoutsProgramName } from './travelpayoutsProgramAliases.js';
