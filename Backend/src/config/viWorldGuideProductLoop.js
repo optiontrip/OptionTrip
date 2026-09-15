e237f31927431e0e6a6ab42446fc91113a3028da
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:13:38 -0700

    Define OptionTrip Vi product loop

---
+++ Backend/src/config/viWorldGuideProductLoop.js
@@ -0,0 +1,14 @@
+export const VI_WORLD_GUIDE_PRODUCT_LOOP = Object.freeze([
+  'understand_intent',
+  'discover_realistic_options',
+  'compare_traveler_value',
+  'assemble_trip',
+  'attach_real_services',
+  'prepare_traveler',
+  'assist_live',
+  'remember_trip',
+  'learn_preferences',
+  'bring_traveler_back',
+]);
+
+export default VI_WORLD_GUIDE_PRODUCT_LOOP;
