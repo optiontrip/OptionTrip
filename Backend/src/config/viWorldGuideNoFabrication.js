9dc4a6503b186de110a67a594c14c3b330cde2a6
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:16:38 -0700

    Add explicit Vi no-fabrication assertions

---
+++ Backend/src/config/viWorldGuideNoFabrication.js
@@ -0,0 +1,12 @@
+export const VI_NO_FABRICATION_ASSERTIONS = Object.freeze([
+  'no_fake_prices',
+  'no_fake_availability',
+  'no_fake_schedules',
+  'no_fake_ratings_or_reviews',
+  'no_fake_booking_confirmation',
+  'no_fake_provider_access',
+  'no_fake_visa_or_entry_rules',
+  'no_fake_live_conditions',
+]);
+
+export default VI_NO_FABRICATION_ASSERTIONS;
