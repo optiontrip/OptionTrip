5efa3801340ce20f87d942cb69a18c11de33a8af
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:10:51 -0700

    Add Vi world-guide product principles

---
+++ Backend/src/config/viWorldGuidePrinciples.js
@@ -0,0 +1,14 @@
+export const VI_WORLD_GUIDE_PRINCIPLES = Object.freeze([
+  'understand_the_person_before_searching_inventory',
+  'turn_emotion_and_constraints_into_realistic_trip_candidates',
+  'cover_the_whole_trip_not_only_flights_and_hotels',
+  'use_live_provider_data_when_claiming_live_commercial_facts',
+  'help_even_when_no_commercial_provider_exists',
+  'rank_for_traveler_value_before_commission',
+  'remember_known_context_and_trip_history',
+  'stay_useful_during_the_trip',
+  'preserve_the_trip_after_return_with_privacy_controls',
+  'automate_everything_that_does_not_require_owner_authority',
+]);
+
+export default VI_WORLD_GUIDE_PRINCIPLES;
