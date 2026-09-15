0ebeb30ce9602acd6cfb38c133b418c6abc0bdd2
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:18:12 -0700

    Define Vi provider fallback behavior

---
+++ Backend/src/config/viWorldGuideProviderFallback.js
@@ -0,0 +1,9 @@
+export const VI_WORLD_GUIDE_PROVIDER_FALLBACK = Object.freeze([
+  'try_another_enabled_provider_for_same_capability',
+  'preserve_known_trip_context',
+  'state_live_data_limit_clearly',
+  'offer_reliable_non_live_guidance_when_useful',
+  'never_replace_missing_live_data_with_fabricated_inventory',
+]);
+
+export default VI_WORLD_GUIDE_PROVIDER_FALLBACK;
