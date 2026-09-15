38d2b380af56154ba4ef19419f48e42380383437
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:14:16 -0700

    Define Vi travel decision order

---
+++ Backend/src/config/viWorldGuideDecisionOrder.js
@@ -0,0 +1,11 @@
+export const VI_WORLD_GUIDE_DECISION_ORDER = Object.freeze([
+  'safety_and_legality',
+  'traveler_constraints',
+  'intent_and_emotion_fit',
+  'live_provider_truth',
+  'total_trip_value',
+  'convenience_and_flexibility',
+  'commercial_attribution',
+]);
+
+export default VI_WORLD_GUIDE_DECISION_ORDER;
