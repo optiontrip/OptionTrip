03eb18ab2d800acdf37cc3c1e4193793b91cd8e9
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:16:58 -0700

    Define Vi commercial service priorities

---
+++ Backend/src/config/viWorldGuideCommercialServices.js
@@ -0,0 +1,14 @@
+export const VI_WORLD_GUIDE_COMMERCIAL_SERVICES = Object.freeze([
+  'flights',
+  'stays',
+  'rail_bus',
+  'rental_cars',
+  'transfers',
+  'activities',
+  'esim',
+  'insurance',
+  'package_tours',
+  'cruises',
+]);
+
+export default VI_WORLD_GUIDE_COMMERCIAL_SERVICES;
