d25ca6843dc8d9b1eabff268cea8f467855a82b7
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:12:08 -0700

    Define Vi automation-first operations contract

---
+++ Backend/src/config/viAutomationContract.js
@@ -0,0 +1,11 @@
+export const VI_AUTOMATION_CONTRACT = Object.freeze({
+  providerHealthChecks: true,
+  stalePriceChecks: true,
+  affiliateLinkChecks: true,
+  routeAndDeadLinkChecks: true,
+  authRegressionChecks: true,
+  seoIndexabilityChecks: true,
+  ownerAlertsOnlyWhenAuthorityRequired: true,
+});
+
+export default VI_AUTOMATION_CONTRACT;
