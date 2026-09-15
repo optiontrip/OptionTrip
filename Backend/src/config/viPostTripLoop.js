3cd398f6ec5b44def3185f3f0d27e94099703e13
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:09:12 -0700

    Define Vi post-trip return loop

---
+++ Backend/src/config/viPostTripLoop.js
@@ -0,0 +1,11 @@
+export const VI_POST_TRIP_LOOP = Object.freeze([
+  'mark_trip_completed',
+  'preserve_visited_places_with_consent',
+  'update_travel_map',
+  'capture_feedback_and_reviews',
+  'build_trip_story_or_memory',
+  'learn_preferences',
+  'use_history_for_next_discovery',
+]);
+
+export default VI_POST_TRIP_LOOP;
