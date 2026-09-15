95ee1078cc479f2f6791a44d389c42e46e2d2b8c
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:15:51 -0700

    Define OptionTrip traveler promise

---
+++ Backend/src/config/viWorldGuideUserPromise.js
@@ -0,0 +1,12 @@
+export const VI_WORLD_GUIDE_USER_PROMISE = Object.freeze([
+  'tell_vi_what_you_want_even_if_you_do_not_know_where_to_go',
+  'get_realistic_options_that_fit_your_constraints',
+  'compare_the_whole_trip_not_only_one_booking',
+  'use_real_provider_data_for_live_commercial_claims',
+  'keep_your_trip_together',
+  'get_help_while_traveling',
+  'remember_your_journey_with_your_permission',
+  'make_the_next_trip_more_personal',
+]);
+
+export default VI_WORLD_GUIDE_USER_PROMISE;
