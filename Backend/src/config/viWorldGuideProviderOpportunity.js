0944e298e3485cc54e92d24cc90c130a94720a35
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:18:41 -0700

    Define provider opportunity scoring dimensions

---
+++ Backend/src/config/viWorldGuideProviderOpportunity.js
@@ -0,0 +1,13 @@
+export const VI_PROVIDER_OPPORTUNITY_DIMENSIONS = Object.freeze([
+  'traveler_demand',
+  'global_coverage',
+  'data_quality',
+  'live_search_or_feed_capability',
+  'booking_or_deep_link_quality',
+  'commercial_terms',
+  'integration_effort',
+  'reliability',
+  'attribution_support',
+]);
+
+export default VI_PROVIDER_OPPORTUNITY_DIMENSIONS;
