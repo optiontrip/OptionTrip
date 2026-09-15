18c9bdff9ccd394f5147838daee94ccd021f1a59
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:13:28 -0700

    Group Vi providers by traveler capability

---
+++ Backend/src/config/viWorldGuideProviderGroups.js
@@ -0,0 +1,10 @@
+export const VI_WORLD_GUIDE_PROVIDER_GROUPS = Object.freeze({
+  air: ['travelpayouts', 'amadeus', 'duffel'],
+  stays: ['travelpayouts', 'hotelbeds'],
+  ground_intercity: ['omio'],
+  experiences: ['tiqets', 'wegotrip', 'viator'],
+  connectivity: ['airalo'],
+  transfers: ['gettransfer'],
+});
+
+export default VI_WORLD_GUIDE_PROVIDER_GROUPS;
