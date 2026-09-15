a822da362b6598446416ec08ce74165ca0c7ac19
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:14:51 -0700

    Add executable Vi roadmap priorities

---
+++ Backend/src/config/viWorldGuideRoadmap.js
@@ -0,0 +1,14 @@
+export const VI_WORLD_GUIDE_ROADMAP = Object.freeze([
+  'preserve_and_regression_test_current_flight_hotel_tools',
+  'wire_world_guide_prompt_behind_flag',
+  'enable_destination_free_discovery',
+  'activate_verified_travelpayouts_provider_capabilities',
+  'persist_multi_service_trip_selections',
+  'connect_trip_object_to_travel_map',
+  'expand_live_trip_assistance',
+  'close_post_trip_memory_return_loop',
+  'expand_provider_catalog_and_attribution',
+  'automate_health_quality_and_integrity_checks',
+]);
+
+export default VI_WORLD_GUIDE_ROADMAP;
