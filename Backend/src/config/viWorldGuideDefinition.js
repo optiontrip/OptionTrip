cd90574df21a8928545b6ce1dc74ffd5c5a1645e
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:16:00 -0700

    Add complete Vi product definition

---
+++ Backend/src/config/viWorldGuideDefinition.js
@@ -0,0 +1,11 @@
+import { VI_WORLD_GUIDE_FINAL_CONTRACT } from './viWorldGuideFinalContract.js';
+import { VI_PROVIDER_EXPANSION_TIERS } from './viWorldGuideProviderExpansion.js';
+import { VI_WORLD_GUIDE_USER_PROMISE } from './viWorldGuideUserPromise.js';
+
+export const VI_WORLD_GUIDE_DEFINITION = Object.freeze({
+  contract: VI_WORLD_GUIDE_FINAL_CONTRACT,
+  providerExpansion: VI_PROVIDER_EXPANSION_TIERS,
+  userPromise: VI_WORLD_GUIDE_USER_PROMISE,
+});
+
+export default VI_WORLD_GUIDE_DEFINITION;
