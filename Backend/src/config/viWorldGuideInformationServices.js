0c0f2d82d7426ad2300bc26a53f926fabf2ffbdf
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:17:07 -0700

    Define Vi noncommercial travel guidance capabilities

---
+++ Backend/src/config/viWorldGuideInformationServices.js
@@ -0,0 +1,14 @@
+export const VI_WORLD_GUIDE_INFORMATION_SERVICES = Object.freeze([
+  'entry_and_visa_guidance',
+  'safety',
+  'weather',
+  'local_transport_guidance',
+  'airport_station_logistics',
+  'baggage_guidance',
+  'disruption_guidance',
+  'restaurant_discovery',
+  'packing_and_preparation',
+  'culture_etiquette_and_tipping',
+]);
+
+export default VI_WORLD_GUIDE_INFORMATION_SERVICES;
