c54b2894a1c4ea7cb91c6d7a0d7688cecdc1f91f
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:10:29 -0700

    Define Vi world-guide product metrics

---
+++ Backend/src/config/viWorldGuideMetrics.js
@@ -0,0 +1,15 @@
+export const VI_WORLD_GUIDE_METRICS = Object.freeze([
+  'destination_free_discovery_to_trip',
+  'trip_to_live_search',
+  'multi_service_trip_attachment',
+  'provider_search_success',
+  'provider_stale_or_failed_inventory',
+  'booking_click_conversion',
+  'trip_completion',
+  'travel_map_update',
+  'return_trip_creation',
+  'context_reask_rate',
+  'live_trip_helpfulness',
+]);
+
+export default VI_WORLD_GUIDE_METRICS;
