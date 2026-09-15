043c8543f26b6981afc607b1758dece0dd813364
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:17:54 -0700

    Define Vi whole-trip comparison dimensions

---
+++ Backend/src/config/viWorldGuideComparison.js
@@ -0,0 +1,15 @@
+export const VI_WORLD_GUIDE_COMPARISON_DIMENSIONS = Object.freeze([
+  'total_trip_cost',
+  'door_to_door_time',
+  'number_of_connections',
+  'baggage_and_fare_rules',
+  'cancellation_and_refundability',
+  'stay_location',
+  'ground_transfer_burden',
+  'accessibility',
+  'entry_constraints',
+  'safety_constraints',
+  'traveler_preference_fit',
+]);
+
+export default VI_WORLD_GUIDE_COMPARISON_DIMENSIONS;
