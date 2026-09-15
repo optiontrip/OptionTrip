80e6bda02979021bd721dea38643ad01f792c4e5
Vitaly Ataev Troshin <optiontripcom@gmail.com>
2026-09-15 09:16:09 -0700

    Add Vi module import smoke check

---
+++ Backend/scripts/checkViWorldGuideImports.js
@@ -0,0 +1,6 @@
+import '../src/config/viWorldGuideDefinition.js';
+import '../src/config/viWorldGuideRuntime.js';
+import '../src/config/viWorldGuideSafetyGate.js';
+import '../src/config/viWorldGuideExports.js';
+
+console.log('Vi world-guide modules import successfully');
